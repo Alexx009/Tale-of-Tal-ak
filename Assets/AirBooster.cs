2d326ea7041b723d589974a0921c69c3d5949e76
k-u-z-u <105111978+k-u-z-u@users.noreply.github.com>
2023-05-07 18:10:58 +0800

    stage 3 daming bugs

--- Assets/AirBooster.cs
+++ Assets/AirBooster.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AirBooster : MonoBehaviour
-{   [SerializeField] private GameObject projectilePrefab; // The projectile prefab to instantiate
+{
+    [SerializeField] private GameObject projectilePrefab; // The projectile prefab to instantiate
     [SerializeField] private Transform spawnPoint; // The position to spawn the projectile from
-    [SerializeField] private float projectileForce = 10f; // The force with which to fire the projectile
+    [SerializeField] private float projectileForce = 5f; // The force with which to fire the projectile
     [SerializeField] private float fireRate = 1f; // The rate of fire (in seconds)
     private float projectileLifeTime = 5f;
-    public float knockbackForce = 250f;
+    public float knockbackForce = 1500f;
     public float knockbackDuration = 0.5f;
 
     private CharacterController characterController;
@@ -17,32 +19,35 @@ public class AirBooster : MonoBehaviour
 
     public ParticleSystem particleEffect;
 
-    // Update is called once per frame
-    void Update()
+    private bool playerIsPresent = false; // Indicates whether the player is currently in the trigger area
+
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("airGun")){
-        StartCoroutine(AirGun());
-        }
-    }
-    private void OnTriggerExit(Collider other) {
-       
-        if(other.CompareTag("airGun")){
-             StopCoroutine(AirGun());
+        if (other.CompareTag("airGun"))
+        {
+            playerIsPresent = true; // Set playerIsPresent to true when the player enters the trigger area
+            StartCoroutine(AirGun());
         }
     }
 
-     private IEnumerator AirGun()
+    private void OnTriggerExit(Collider other)
     {
-        while (true)
+        if (other.CompareTag("airGun"))
+        {
+            playerIsPresent = false; // Set playerIsPresent to false when the player exits the trigger area
+            StopCoroutine(AirGun());
+        }
+    }
+
+    private IEnumerator AirGun()
+    {
+        while (playerIsPresent) // Only run the coroutine while the player is present
         {
             // Play the warning particle effect
             particleEffect.Play();
 
             // Wait for a short duration to allow the warning effect to finish
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(3f);
 
             // Instantiate the projectile prefab at the spawn point position
             GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
@@ -57,7 +62,7 @@ public class AirBooster : MonoBehaviour
             particleEffect.Stop();
 
             // Wait for the specified fire rate before firing the next projectile
-            yield return new WaitForSeconds(10);
+            yield return new WaitForSeconds(fireRate);
         }
     }
 }
