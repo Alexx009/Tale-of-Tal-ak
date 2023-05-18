c50fb526e13d14bd7665b114ab6b2090bb608fce
k-u-z-u <105111978+k-u-z-u@users.noreply.github.com>
2023-05-18 14:28:32 +0800

    stage 3 design + sfx ng turret at octopus

---
+++ Assets/RandomGrowlSound.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomGrowlSound : MonoBehaviour
+{
+    public List<AudioClip> growlSounds;
+    public float minDistance = 10f; // Minimum distance for the sound to be heard clearly
+    public float maxDistance = 20f; // Maximum distance at which the sound can be heard
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.spatialBlend = 1f; // Set to 3D audio
+        audioSource.minDistance = minDistance;
+        audioSource.maxDistance = maxDistance;
+
+        StartCoroutine(PlayGrowlWithDelay());
+    }
+
+    private IEnumerator PlayGrowlWithDelay()
+    {
+        while (true)
+        {
+            PlayRandomGrowl();
+            yield return new WaitForSeconds(Random.Range(3f, 10f)); // Adjust the delay as desired
+        }
+    }
+
+    public void PlayRandomGrowl()
+    {
+        if (growlSounds.Count == 0)
+        {
+            Debug.LogWarning("No growl sounds assigned!");
+            return;
+        }
+
+        int randomIndex = Random.Range(0, growlSounds.Count);
+        AudioClip randomClip = growlSounds[randomIndex];
+        audioSource.PlayOneShot(randomClip);
+    }
+}
