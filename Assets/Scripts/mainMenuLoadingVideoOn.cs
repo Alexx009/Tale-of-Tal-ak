26f3ab50351f55339dc9820353024ed9edd3bd2d
Alexis Desiatco <99580667+Alexx009@users.noreply.github.com>
2023-05-28 20:33:07 +0800

    Changes in CutScenes

--- Assets/Scripts/mainMenuLoadingVideoOn.cs
+++ Assets/Scripts/mainMenuLoadingVideoOn.cs
@@ -7,16 +7,18 @@ public class mainMenuLoadingVideoOn : MonoBehaviour
     public GameObject loadingVideo;
     public mainMenuElementAnimation mainMenuElementAnimation;
     // Start is called before the first frame update
-    void Awake()
-    {
-        loadingVideo.SetActive(true);
-        mainMenuElementAnimation.enabled = false;
+    // void Awake()
+    // {
         
-    }
+        
+        
+    // }
 
 
     private void Start()
     {
+        loadingVideo.SetActive(true);
+        mainMenuElementAnimation.enabled = false;
         Invoke("TurnOffTheVideo", 3f);
     }
 
